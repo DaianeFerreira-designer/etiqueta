261e12a02a641dc216ee4912464220b4f36bb5a1
Daiane Ferreira <161055790+DaianeFerreira-designer@users.noreply.github.com>
2024-10-22 12:30:09 -0300

    ignore

---
+++ token.js
@@ -0,0 +1,14 @@
+// token.js
+const TINY_API_TOKEN = 'ce53a026fe501c6b7bec1154d5722e3acf3e7ab4e3d27ece8d541ee8cbc147e5';
+const GOOGLE_SHEETS_CLIENT_ID = '284870043528-93t52t606sbbv5rgqrrl6u7ueqa8gdpd.apps.googleusercontent.com';
+const GOOGLE_SHEETS_CLIENT_SECRET = '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';
+const GOOGLE_SHEETS_REDIRECT_URI = 'http://localhost';
+const GOOGLE_SHEETS_REFRESH_TOKEN = 'SEU_REFRESH_TOKEN_AQUI'; // Substitua pelo refresh token que você irá obter
+
+module.exports = {
+  TINY_API_TOKEN,
+  GOOGLE_SHEETS_CLIENT_ID,
+  GOOGLE_SHEETS_CLIENT_SECRET,
+  GOOGLE_SHEETS_REDIRECT_URI,
+  GOOGLE_SHEETS_REFRESH_TOKEN,
+};
